b62d6e550e92d520546e215564772bbc828c8203
Brasero <brandonricci4@gmail.com>
2021-08-12 17:01:05 +0200

    adding style to Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,22 @@
+import '../styles/Cart.css'
+
 function Cart() {
        const item1 = 'Monstera'
        const item2 = 'Lierres'
+       const item3 = "bouquet de fleurs"
        const prix1 = 8
        const prix2 = 10
+       const prix3 = 15
     return (
-        <div>
+        <div className="lmj-cart">
             <h2>Panier</h2>
             <ul>
                 <li>{ item1 } : <small>{ prix1 }€</small></li>
                 <li>{ item2 } : <small>{ prix2 }€</small></li>
+                <li>{ item3 } : <small>{ prix3 }€</small></li>
             </ul>
             <div>
-                Total : { prix1 + prix2 }€
+                Total : { prix1 + prix2 + prix3 }€
             </div>
         </div>
     )
